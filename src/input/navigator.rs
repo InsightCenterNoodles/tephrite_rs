0acafa92ac70f397ac35e574d765d51a2fe3f968
Nicholas Brunhart-Lupo <nicholas.brunhart-lupo@nrel.gov>
2026-03-04 09:59:11 -0700

    Allow multiple nav markers

--- src/input/navigator.rs
+++ src/input/navigator.rs
@@ -39,95 +39,90 @@ impl Plugin for NavigationPlugin {
 }
 
 fn on_tick(
-    mut target: Query<
-        (&mut Transform, Option<&ChildOf>),
-        (With<NavigatorMarker>, Without<Interactor>),
-    >,
+    target: Query<(&mut Transform, Option<&ChildOf>), (With<NavigatorMarker>, Without<Interactor>)>,
     mut joystick: Query<(&GlobalTransform, &InteractorState), With<Interactor>>,
     parents: Query<&GlobalTransform>,
     settings: Res<NavigatorSettings>,
     time: Res<Time>,
 ) {
-    let Ok((mut target_tf, target_parent)) = target.single_mut() else {
-        return;
-    };
-
-    // TODO: will fail if we add more interactors
-    let Ok((joystick_global_tf, state)) = joystick.single_mut() else {
-        return;
-    };
-
-    let speed_meters_per_second = 2.0;
-    let rotation_degrees_per_second = 40.0;
-
-    let joy_world_position = joystick_global_tf.transform_point(Vec3::ZERO);
-
-    if let Some(right_stick) = state.stick_state(JoystickType::Right) {
-        // direction vector
-
-        let dir = vec3(right_stick.x, 0.0, right_stick.y);
-        let mut global_dir = joystick_global_tf.affine().transform_vector3(dir);
-
-        // flatten that y!
-        global_dir.y = 0.0;
-
-        let global_displace = global_dir * speed_meters_per_second * time.delta_secs();
-
-        // map to the local of the entity
-
-        let parent_global_affine = target_parent
-            .and_then(|parent| parents.get(parent.0).ok())
-            .map(|parent_tf| parent_tf.affine())
-            .unwrap_or_else(|| GlobalTransform::IDENTITY.affine());
-
-        let local_displace = parent_global_affine
-            .inverse()
-            .transform_vector3(global_displace);
-
-        target_tf.translation += local_displace;
-    }
-
-    if let Some(left_stick) = state.stick_state(JoystickType::Left) {
-        target_tf.translation += vec3(
-            0.0,
-            time.delta_secs() * speed_meters_per_second * left_stick.y,
-            0.0,
-        )
-    }
-
-    if let Some(dpad) = state.stick_state(JoystickType::DPad) {
-        let degrees = dpad.x;
-
-        // left is 270 ish and right is 90 ish
-
-        let dir = match degrees {
-            255.0..285.0 => -1.0,
-            75.0..105.0 => 1.0,
-            _ => 0.0,
+    for (mut target_tf, target_parent) in target {
+        // TODO: will fail if we add more interactors
+        let Ok((joystick_global_tf, state)) = joystick.single_mut() else {
+            return;
         };
 
-        target_tf.rotate_axis(
-            Dir3::Y,
-            (rotation_degrees_per_second * time.delta_secs() * dir).to_radians(),
-        );
-    }
+        let speed_meters_per_second = 2.0;
+        let rotation_degrees_per_second = 40.0;
 
-    const SCALE_FACTOR: f32 = 1.01;
+        let joy_world_position = joystick_global_tf.transform_point(Vec3::ZERO);
 
-    if state.buttons.pressed(super::JoystickButton::BL) {
-        target_tf.scale =
-            (target_tf.scale / SCALE_FACTOR).clamp(Vec3::splat(0.001), Vec3::splat(1000.0));
-    }
+        if let Some(right_stick) = state.stick_state(JoystickType::Right) {
+            // direction vector
 
-    if state.buttons.pressed(super::JoystickButton::BR) {
-        target_tf.scale =
-            (target_tf.scale * SCALE_FACTOR).clamp(Vec3::splat(0.001), Vec3::splat(1000.0));
-    }
+            let dir = vec3(right_stick.x, 0.0, right_stick.y);
+            let mut global_dir = joystick_global_tf.affine().transform_vector3(dir);
 
-    if state
-        .buttons
-        .just_pressed(crate::input::JoystickButton::Start)
-    {
-        *target_tf = Transform::IDENTITY;
+            // flatten that y!
+            global_dir.y = 0.0;
+
+            let global_displace = global_dir * speed_meters_per_second * time.delta_secs();
+
+            // map to the local of the entity
+
+            let parent_global_affine = target_parent
+                .and_then(|parent| parents.get(parent.0).ok())
+                .map(|parent_tf| parent_tf.affine())
+                .unwrap_or_else(|| GlobalTransform::IDENTITY.affine());
+
+            let local_displace = parent_global_affine
+                .inverse()
+                .transform_vector3(global_displace);
+
+            target_tf.translation += local_displace;
+        }
+
+        if let Some(left_stick) = state.stick_state(JoystickType::Left) {
+            target_tf.translation += vec3(
+                0.0,
+                time.delta_secs() * speed_meters_per_second * left_stick.y,
+                0.0,
+            )
+        }
+
+        if let Some(dpad) = state.stick_state(JoystickType::DPad) {
+            let degrees = dpad.x;
+
+            // left is 270 ish and right is 90 ish
+
+            let dir = match degrees {
+                255.0..285.0 => -1.0,
+                75.0..105.0 => 1.0,
+                _ => 0.0,
+            };
+
+            target_tf.rotate_axis(
+                Dir3::Y,
+                (rotation_degrees_per_second * time.delta_secs() * dir).to_radians(),
+            );
+        }
+
+        const SCALE_FACTOR: f32 = 1.01;
+
+        if state.buttons.pressed(super::JoystickButton::BL) {
+            target_tf.scale =
+                (target_tf.scale / SCALE_FACTOR).clamp(Vec3::splat(0.001), Vec3::splat(1000.0));
+        }
+
+        if state.buttons.pressed(super::JoystickButton::BR) {
+            target_tf.scale =
+                (target_tf.scale * SCALE_FACTOR).clamp(Vec3::splat(0.001), Vec3::splat(1000.0));
+        }
+
+        if state
+            .buttons
+            .just_pressed(crate::input::JoystickButton::Start)
+        {
+            *target_tf = Transform::IDENTITY;
+        }
     }
 }
